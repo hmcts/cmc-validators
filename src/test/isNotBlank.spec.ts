9b3d3fe577b0651834f70c0ced680f39f1dec9a4
Ellie Harrison <ellieh@kainos.com>
2018-05-01 09:35:58 +0100

    ROC-3671: Add isBooleanTrue validator (#8)
    * ROC-3671 Update existing tests for 100% coverage
    
    * ROC-3671 Add Boolean True validator
    
    * Rename isBooleanConstraint to isBooleanTrue

--- src/test/isNotBlank.spec.ts
+++ src/test/isNotBlank.spec.ts
@@ -1,36 +1,45 @@
 import { expect } from 'chai'
-import { IsNotBlankConstraint } from '../main/isNotBlank'
+import { IsNotBlank } from '../main/isNotBlank'
+import { validateSync } from 'class-validator'
+
+class NotBlankTest {
+  @IsNotBlank()
+  value?: any
+
+  constructor (value?: any) {
+    this.value = value
+  }
+}
 
 describe('IsNotBlank', () => {
-  const constraint: IsNotBlankConstraint = new IsNotBlankConstraint()
 
   describe('validate', () => {
 
     describe('should return true when ', () => {
 
       it('given an undefined value', () => {
-        expect(constraint.validate(undefined)).to.be.equal(true)
+        expect(validateSync(new NotBlankTest(undefined))).to.be.length(0)
       })
 
       it('given ab non blank value', () => {
-        expect(constraint.validate('something')).to.be.equal(true)
+        expect(validateSync(new NotBlankTest('Something'))).to.be.length(0)
       })
     })
 
     describe('should return false when ', () => {
 
       it('given an non string value', () => {
-        expect(constraint.validate(true)).to.be.equal(false)
-        expect(constraint.validate(999)).to.be.equal(false)
-        expect(constraint.validate({})).to.be.equal(false)
+        expect(validateSync(new NotBlankTest(true))).to.be.length(1)
+        expect(validateSync(new NotBlankTest(999))).to.be.length(1)
+        expect(validateSync(new NotBlankTest({}))).to.be.length(1)
       })
 
       it('given an empty value', () => {
-        expect(constraint.validate('')).to.be.equal(false)
+        expect(validateSync(new NotBlankTest(''))).to.be.length(1)
       })
 
       it('given an blank value', () => {
-        expect(constraint.validate(' ')).to.be.equal(false)
+        expect(validateSync(new NotBlankTest(' '))).to.be.length(1)
       })
 
     })
