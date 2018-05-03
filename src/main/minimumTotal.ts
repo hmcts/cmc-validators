436c5f9d29c340200ebc061f6d425f2d40a11e93
Ellie Harrison <ellieh@kainos.com>
2018-05-03 16:13:58 +0100

    Add MaxLength, MinValue and MinTotal validators (#10)

---
+++ src/main/minimumTotal.ts
@@ -0,0 +1,67 @@
+import {
+  registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface
+} from 'class-validator'
+
+@ValidatorConstraint()
+export class MinTotalConstraint implements ValidatorConstraintInterface {
+
+  validate (value: any, args: ValidationArguments): boolean {
+    if (value === undefined) {
+      return true
+    }
+
+    if (!Array.isArray(value)) {
+      throw new Error('Expected validated element to be an array')
+    }
+
+    const minValue: number = this.extractMinValue(args)
+    let total: number = 0
+    for (let row of value) {
+      if (typeof row.amount === 'number') {
+        total += row.amount
+      }
+    }
+
+    return total >= minValue
+  }
+
+  private extractMinValue (args: ValidationArguments): number {
+    let minValue = args.constraints[0]
+    if (typeof minValue === 'string') {
+      minValue = (args.object as any)[minValue]
+    }
+    if (typeof minValue !== 'number') {
+      throw new Error('Minimal required value parameter not given')
+    }
+
+    return minValue
+  }
+}
+
+/**
+ * Marks an array of objects with an amount parameter to be determined as valid if meeting a target amount
+ * @param {any} minValue, the numeric value, or property name to a numeric value on annotated object that must be met
+ * @param {ValidationOptions} validationOptions
+ *
+ * @example
+ * class MinTotalTest {
+ *  <at>MinTotal('targetValue')
+ *  value?: any[] = [ { amount: 1 }, { amount: 2 } ]
+ *  targetValue: number = 4
+ * }
+ * minimum: MinTotalTest = new MinTotalTest(25)
+ * //Will return a validation error as the sum of amounts (3) is less than the target of 4 than 3
+ * errors: ValidationError[] = validateSync(minimum)
+ */
+export function MinTotal (minValue: any, validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'MinTotal',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [minValue],
+      validator: MinTotalConstraint
+    })
+  }
+}
---
+++ src/test/minimumValue.spec.ts
@@ -0,0 +1,42 @@
+import { expect } from 'chai'
+import { Min } from '../main/minimumValue'
+import { validateSync } from 'class-validator'
+
+const MINIMUM_VALUE: number = 200
+
+class MinimumValueTest {
+  @Min(MINIMUM_VALUE)
+  value: any
+
+  constructor (value?: any) {
+    this.value = value
+  }
+}
+
+describe('Min', () => {
+
+  describe('validate', () => {
+
+    context('should return no error when', () => {
+
+      it('given an undefined value', () => {
+        expect(validateSync(new MinimumValueTest(undefined))).to.be.empty
+      })
+
+      it('given value is greater than min value', () => {
+        expect(validateSync(new MinimumValueTest(MINIMUM_VALUE + 1))).to.be.empty
+      })
+
+      it('given value is equal than min value', () => {
+        expect(validateSync(new MinimumValueTest(MINIMUM_VALUE))).to.be.empty
+      })
+    })
+
+    context('should return a validation error when', () => {
+
+      it('given value is lower than min value', () => {
+        expect(validateSync(new MinimumValueTest(MINIMUM_VALUE - 1))).to.not.be.empty
+      })
+    })
+  })
+})
