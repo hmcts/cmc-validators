516af43c40860373d8222958513d34ec71ce0e22
Andrew Walker <andrewwa@kainos.com>
2019-12-16 17:17:30 +0000

    Update email verification algorithm to match GOV Notify's

--- src/main/isEmail.ts
+++ src/main/isEmail.ts
@@ -1,20 +1,60 @@
 import {
   registerDecorator,
+  ValidationArguments,
   ValidationOptions,
   ValidatorConstraint,
-  ValidatorConstraintInterface,
-  ValidationArguments
+  ValidatorConstraintInterface
 } from '@hmcts/class-validator'
-
-import * as validator from 'validator'
+import * as punycode from 'punycode'
 
 @ValidatorConstraint()
 export class IsEmailConstraint implements ValidatorConstraintInterface {
+  private static EMAIL_REGEX = new RegExp('^[a-z0-9.!#$%&\'*+/=?^_`{|}~\\-]+@([^.@][^@\\s]+)$', 'i')
+  private static HOSTNAME_PART_REGEX = new RegExp('^(xn-|[a-z0-9]+)(-[a-z0-9]+)*$', 'i')
+  private static TLD_REGEX = new RegExp('^([a-z]{2,63}|xn--([a-z0-9]+-)*[a-z0-9]+)$', 'i')
 
-  validate (value: any, args: ValidationArguments): boolean {
-    return typeof value === 'string' && (value.length === 0 || validator.isEmail(value))
+  public static isEmail (value: string): boolean {
+    if (value.length > 320) {
+      return false
+    }
+    if (value.includes('..')) {
+      return false
+    }
+    let match = IsEmailConstraint.EMAIL_REGEX.exec(value)
+    if (!match) {
+      return false
+    }
+    let hostname = match[1]
+    try {
+      hostname = punycode.toASCII(hostname)
+      punycode.toUnicode(hostname)
+    } catch (err) {
+      return false
+    }
+    if (hostname.length > 253) {
+      return false
+    }
+    let parts: string[] = hostname.split('.')
+
+    if (parts.length < 2) {
+      return false
+    }
+    for (let part of parts) {
+      if (part.length > 63) {
+        return false
+      }
+      if (!IsEmailConstraint.HOSTNAME_PART_REGEX.exec(part)) {
+        return false
+      }
+    }
+
+    let tld = parts[parts.length - 1]
+    return !!IsEmailConstraint.TLD_REGEX.exec(tld)
   }
 
+  validate (value: any, args: ValidationArguments): boolean {
+    return typeof value === 'string' && (value.length === 0 || IsEmailConstraint.isEmail(value))
+  }
 }
 
 /**
--- src/test/isEmail.spec.ts
+++ src/test/isEmail.spec.ts
@@ -11,36 +11,95 @@ class EmailTest {
   }
 }
 
+const validEmailAddresses = [
+  'email@domain.com',
+  'email@domain.COM',
+  'firstname.lastname@domain.com',
+  'firstname.o\'lastname@domain.com',
+  'email@subdomain.domain.com',
+  'firstname+lastname@domain.com',
+  '1234567890@domain.com',
+  'email@domain-one.com',
+  '_______@domain.com',
+  'email@domain.name',
+  'email@domain.superlongtld',
+  'email@domain.co.jp',
+  'firstname-lastname@domain.com',
+  'info@german-financial-services.vermögensberatung',
+  'info@german-financial-services.reallylongarbitrarytldthatiswaytoohugejustincase',
+  'japanese-info@例え.テスト'
+]
+
+const invalidEmailAddresses = [
+  'email@123.123.123.123',
+  'email@[123.123.123.123]',
+  'plainaddress',
+  '@no-local-part.com',
+  'Outlook Contact <outlook-contact@domain.com>',
+  'no-at.domain.com',
+  'no-tld@domain',
+  ';beginning-semicolon@domain.co.uk',
+  'middle-semicolon@domain.co;uk',
+  'trailing-semicolon@domain.com;',
+  '"email+leading-quotes@domain.com',
+  'email+middle"-quotes@domain.com',
+  '"quoted-local-part"@domain.com',
+  '"quoted@domain.com"',
+  'lots-of-dots@domain..gov..uk',
+  'two-dots..in-local@domain.com',
+  'multiple@domains@domain.com',
+  'spaces in local@domain.com',
+  'spaces-in-domain@dom ain.com',
+  'underscores-in-domain@dom_ain.com',
+  'pipe-in-domain@example.com|gov.uk',
+  'comma,in-local@gov.uk',
+  'comma-in-domain@domain,gov.uk',
+  'pound-sign-in-local£@domain.com',
+  'local-with-’-apostrophe@domain.com',
+  'local-with-”-quotes@domain.com',
+  'domain-starts-with-a-dot@.domain.com',
+  'brackets(in)local@domain.com',
+  'email-too-long-' + 'a'.repeat(320) + '@example.com'
+]
+
 describe('IsEmail', () => {
-
-  describe('validate', () => {
-
-    describe('should have no validation errors when', () => {
-      it('given a valid email', () => {
-        expect(validateSync(new EmailTest('my-test@example.com'))).to.be.empty
-      })
-
-      it('given an empty string', () => {
-        expect(validateSync(new EmailTest(''))).to.be.empty
+  describe('should have no validation errors when', () => {
+    context('given a valid email address', () => {
+      validEmailAddresses.forEach(address => {
+        it(address, () => {
+          expect(validateSync(new EmailTest(address)), address).to.be.empty
+        })
       })
     })
 
-    describe('should have a validation error when', () => {
-      it('given an invalid email (string)', () => {
-        expect(validateSync(new EmailTest('aaaaa'))).to.not.be.empty
-      })
+    it('given an empty string', () => {
+      expect(validateSync(new EmailTest(''))).to.be.empty
+    })
+  })
 
-      it('given a number', () => {
-        expect(validateSync(new EmailTest(123))).to.not.be.empty
+  describe('should have a validation error when', () => {
+    context('given an invalid email address', () => {
+      invalidEmailAddresses.forEach(address => {
+        it(address, () => {
+          expect(validateSync(new EmailTest(address))).to.not.be.empty
+        })
       })
+    })
 
-      it('given an object', () => {
-        expect(validateSync(new EmailTest({}))).to.not.be.empty
-      })
+    it('given a number', () => {
+      expect(validateSync(new EmailTest(123))).to.not.be.empty
+    })
 
-      it('given null', () => {
-        expect(validateSync(new EmailTest(null))).to.not.be.empty
-      })
+    it('given an object', () => {
+      expect(validateSync(new EmailTest({}))).to.not.be.empty
+    })
+
+    it('given null', () => {
+      expect(validateSync(new EmailTest(null))).to.not.be.empty
+    })
+
+    it('given undefined', () => {
+      expect(validateSync(new EmailTest(undefined))).to.not.be.empty
     })
   })
 })
